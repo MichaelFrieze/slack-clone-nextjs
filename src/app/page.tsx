85f33b67cbed83fd2d9d50a8f2e7e9609932955d
Michael Frieze <michaelfrieze@icloud.com>
2024-09-11 15:43:49 -0400

    feat: auth UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl font-bold text-rose-500">Home page</h1>
-      <Button variant={"destructive"}>Click Me</Button>
+      <AuthScreen />
     </div>
   );
 }
---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { TriangleAlert } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+
+import { SignInFlow } from "../types";
+
+interface SignInCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+export const SignInCard = ({ setState }: SignInCardProps) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [pending, setPending] = useState(false);
+
+  const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("onPasswordSignIn", email, password);
+  };
+
+  const onProviderSignIn = (value: "github" | "google") => {
+    console.log("onProviderSignIn", value);
+  };
+
+  return (
+    <Card className="h-full w-full p-8">
+      <CardHeader className="px-0 pt-0">
+        <CardTitle>Login to continue</CardTitle>
+        <CardDescription>
+          Use your email or another service to continue
+        </CardDescription>
+      </CardHeader>
+      {!!error && (
+        <div className="mb-6 flex items-center gap-x-2 rounded-md bg-destructive/15 p-3 text-sm text-destructive">
+          <TriangleAlert className="size-4" />
+          <p>{error}</p>
+        </div>
+      )}
+      <CardContent className="space-y-5 px-0 pb-0">
+        <form onSubmit={onPasswordSignIn} className="space-y-2.5">
+          <Input
+            disabled={pending}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            type="email"
+            required
+          />
+          <Input
+            disabled={pending}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            type="password"
+            required
+          />
+          <Button type="submit" className="w-full" size="lg" disabled={pending}>
+            Continue
+          </Button>
+        </form>
+        <Separator />
+        <div className="flex flex-col gap-y-2.5">
+          <Button
+            disabled={pending}
+            onClick={() => onProviderSignIn("google")}
+            variant="outline"
+            size="lg"
+            className="relative w-full"
+          >
+            <FcGoogle className="absolute left-2.5 top-3 size-5" />
+            Continue with Google
+          </Button>
+          <Button
+            disabled={pending}
+            onClick={() => onProviderSignIn("github")}
+            variant="outline"
+            size="lg"
+            className="relative w-full"
+          >
+            <FaGithub className="absolute left-2.5 top-3 size-5" />
+            Continue with Github
+          </Button>
+        </div>
+        <p className="text-xs text-muted-foreground">
+          Don&apos;t have an account?{" "}
+          <span
+            onClick={() => setState("signUp")}
+            className="cursor-pointer text-sky-700 hover:underline"
+          >
+            Sign up
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
